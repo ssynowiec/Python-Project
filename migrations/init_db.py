08140e02704981a2bc747c644f09f81e2f913d7f
Stanisław Synowiec <stasyn1410@gmail.com>
2023-05-27 10:21:26 +0200

    add: Error Exception

--- migrations/init_db.py
+++ migrations/init_db.py
@@ -2,16 +2,20 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
+try:
+    with open('./migrations/schema.sql') as f:
+        connection.executescript(f.read())
 
-with open('./migrations/schema.sql') as f:
-    connection.executescript(f.read())
+    cur = connection.cursor()
 
-cur = connection.cursor()
+    # insert some users
+    cur.execute("INSERT INTO users (username) VALUES (?)", ('admin',))
 
-# insert some users
-cur.execute("INSERT INTO users (username) VALUES (?)", ('admin',))
+    cur.execute("INSERT INTO users (username) VALUES (?)", ('demo',))
 
-cur.execute("INSERT INTO users (username) VALUES (?)", ('demo',))
-
-connection.commit()
-connection.close()
+    connection.commit()
+    print("User table created successfully")
+except Exception as e:
+    print("User table creation failed", e)
+finally:
+    connection.close()
