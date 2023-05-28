b0ca7f5005228e0f359be7c3fc557df3db16dbb8
Stanisław Synowiec <stasyn1410@gmail.com>
2023-05-28 16:13:04 +0200

    add: protection api

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@
 # <Flask's main data package>
 from flask import Flask
 
+from src.utils.EnvSystem import EnvSystem
 from src.utils.FileSystem import FileSystem
 
 app: Flask = Flask(__name__,
@@ -12,7 +13,8 @@ app: Flask = Flask(__name__,
                    template_folder='templates',
                    static_folder='public')
 
-
+SECRET_KEY = EnvSystem.get_env_element('SECRET_KEY')
+app.config['SECRET_KEY'] = SECRET_KEY
 ###########################################################################
 # All files in the <view> directory should go here.
 # Thanks to this, we clearly tell flask that such files are somewhere in the project and we give it
